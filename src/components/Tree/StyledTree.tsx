bef989869bc5288d207df97aab238a105222e636
dimitarrusev <contact@dimitarrusev.com>
2020-05-09 11:29:45 +0200

    feat: add `Tree` component
    closes #6

---
+++ src/components/Tree/StyledTree.tsx
@@ -0,0 +1,9 @@
+// Vendor
+import styled from "styled-components";
+
+const StyledTree = styled.ul`
+  margin: 0;
+  padding: 0;
+`;
+
+export default StyledTree;
---
+++ src/components/Tree/Tree.tsx
@@ -0,0 +1,9 @@
+// Vendor
+import React from "react";
+
+// Components
+import StyledTree from "./StyledTree";
+
+const Tree: React.FC = ({ children }) => <StyledTree>{children}</StyledTree>;
+
+export default Tree;
